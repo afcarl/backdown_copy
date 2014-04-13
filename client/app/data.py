1e58b9c230782ae28551e00039fb4533b037bbaa
nate-parrott <nateparro2t@gmail.com>
2014-04-13 11:29:40 -0700

    geoputngio

--- client/app/data.py
+++ client/app/data.py
@@ -2,11 +2,14 @@ import os
 import json
 import sys
 import threading
+import shutil
 
 conf_lock = threading.Lock()
 
-port = int(sys.argv[1]) if len(sys.argv) > 1 else 9999
+port = int(sys.argv[1]) if (len(sys.argv) > 1 and sys.argv[1][0]!='-') else 9999
 data_dir = "data_"+str(port)
+if '--clear' in sys.argv:
+    shutil.rmtree(data_dir)
 default_dir = os.path.expanduser("~/Desktop/Backdown")
 if not os.path.exists(data_dir):
     os.mkdir(data_dir)
--- client/run.py
+++ client/run.py
@@ -2,8 +2,3 @@ import sys
 import app
 app.app.run(port=app.data.port)
 
-
-
-
-
-
